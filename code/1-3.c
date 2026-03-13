cd61b2475de313a4a648c3af578bd1d2f0dbff02
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-03-13 10:29:05 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
+    int age;
+    char grade;
+    float gpa;
+
+    scanf("%d %c %f",&age,&grade,&gpa);
+    printf("%d %c %f\n",age,grade,gpa);
 
 
     return 0;
