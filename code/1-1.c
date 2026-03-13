43afc14dcb4b643d8c83fbb7e676d761865a1d13
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-03-13 10:15:28 +0900

    1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
+    int var1 = 100;
+    printf("int : %d\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
+
+    char var2 = 'c';
+    printf("char : %c\n",var2);
+
+    char* var3 = "hello world!";
+    printf("String : %s\n",var3);
+
+    float var4 = 3.141592;
+    printf("float : %f\n",var4);
+    printf("exp : %lf\n",var4);
+
 
     return 0;
 }
