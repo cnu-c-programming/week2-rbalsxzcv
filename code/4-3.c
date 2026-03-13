5f18d832e742d17dd0938c68dfce272c4901f018
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-03-13 11:32:46 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-
+int main(){
+    int input;
+    int bol = 1;
+    
+    scanf("%d",&input);
+    for(int i = 2; i<=sizeof(input)/2;i++){
+        if(input % i == 0)
+            bol = 0;
+    }
+    if(bol == 1)
+        printf("true");
+    else
+        printf("false");    
     return 0;
 }
-
