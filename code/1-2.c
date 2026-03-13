6b1dccebd87234e8eb0bfb95e59592e003592573
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-03-13 10:28:51 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
-{
+
+int main(){
+    printf("% 10s % 10s % 10s\n","name","id","value");
+    printf("% 10s %010d % 10.2f\n","kim",12,4.12);
+    printf("% 10s %010d % 10.2f\n","lee",1922,3.21);
+    printf("% 10s %010d % 10.2f\n","park",423,1.0);
+
 
 
     return 0;
